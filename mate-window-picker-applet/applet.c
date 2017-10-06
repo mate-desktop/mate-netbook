06341f8fbb2d0db3929e33f0c9f9c5cf93fb0039
monsta <monsta@inbox.ru>
2017-10-06 12:57:26 +0300

    window picker: update copyright year

--- mate-window-picker-applet/applet.c
+++ mate-window-picker-applet/applet.c
@@ -214,7 +214,8 @@ display_about_dialog (GtkAction       *action,
                 "authors", close_window_authors,
                 "logo-icon-name", "preferences-system-windows",
                 "copyright", "Copyright \xc2\xa9 2008 Canonical Ltd\n"
-                             "Copyright \xc2\xa9 2013-2014 Stefano Karapetsas",
+                             "Copyright \xc2\xa9 2013-2014 Stefano Karapetsas\n"
+                             "Copyright \xc2\xa9 2015-2017 MATE developers",
                 NULL);
 
   gtk_widget_show (panel_about_dialog);
