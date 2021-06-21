779342594bcb19549c61639da0f7760007091a69
raveit65 <mate@raveit.de>
2021-06-21 23:17:44 +0200

    update copyright to 2021

--- maximus/tomboykeybinder.c
+++ maximus/tomboykeybinder.c
@@ -1,5 +1,6 @@
 /* tomboykeybinder.c
  * Copyright (C) 2008 Novell
+ * Copyright (C) 2012-2021 MATE Developers
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
--- maximus/xutils.h
+++ maximus/xutils.h
@@ -4,6 +4,7 @@
 /*
  * Copyright (C) 2001 Havoc Pennington
  * Copyright (C) 2005-2007 Vincent Untz
+ * Copyright (C) 2012-2021 MATE Developers
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
